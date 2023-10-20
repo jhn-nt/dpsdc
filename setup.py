50dba35cd899298f783d847ddb15b7d5e8d2ca20
jhn-nt <angelotti.giovanni.ds@gmail.com>
2023-10-20 17:17:14 +0000

    update in setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 
 setup(
     name="dpsdc",
-    version="0.1",
-    description="Workshop on Social Determinats of Care and Proxies of Disparities",
+    version="0.2",
+    description="Disparity Proxies and Social Determinants of Care",
     author="Adrien Carrel, Giovanni Angelotti, Luca Leon Weishaupt, Lelia Marie Hampton, Nicole Dundas, Pierandrea Morandini",
     packages=["dpsdc"],
     include_package_data=True,
     install_requires=[
         "python-dotenv==1.0.0",
-        "jupyter==1.0.0",
         "pandas==2.1.1",
         "scikit-learn==1.3.1",
         "seaborn==0.13.0",
